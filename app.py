e7a642f12bf79c9448e7bffa4565891d3433efde
bossl0la1 <dejijafo@gmail.com>
2025-06-24 23:49:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -116,17 +116,19 @@ if page == "🏠 Home":
     buf = BytesIO()
     qr.save(buf, format="PNG")
     st.image(buf.getvalue(), width=200, caption="📱 Scan to open app")
-    # HTML code for the video with autoplay
-    video_html = """
-    <video width="80%" height="auto" autoplay>
-        <source src="OGT.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
-    """
+   
 
+   
+    st.image("OGT.jpg", width=100)
+     # HTML code for the video with autoplay
+        video_html = """
+        <video width="80%" height="auto" autoplay>
+            <source src="OGT.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+        """ 
     # Display the video using Streamlit's markdown method
     st.markdown(video_html, unsafe_allow_html=True)
-    st.image("OGT.jpg", width=100)
 
 # ---------- INTRODUCTION ----------
 elif page == "Introduction to Organic chemistry":
